05d49c5330295a1343791cb42a5b5ee3a7d43ef4
luuungu <masauso.l@outlook.com>
2025-02-20 17:28:06 +0100

    making todo list day 1

--- mega_60_days/todo_app/main.py
+++ mega_60_days/todo_app/main.py
@@ -1,5 +1,12 @@
-# 1st program
-print("hello, Lungu")
+# print() function displays text to the console
+#input function promts the user for input.
+user_prompt='Enter a todo: '
+todo1=input(user_prompt)
+todo2=input(user_prompt)
+todo3=input(user_prompt)
 
-#User input and variables
-input()
+#list data type:[]i
+todos=[todo1, todo2, todo3]
+print(todos)
+print(type(todos))
+

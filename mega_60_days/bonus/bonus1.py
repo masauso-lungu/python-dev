edc1e1cf07142ca936ddfd35cfb1336bfa80ba4e
luuungu <masauso.l@outlook.com>
2025-04-09 01:02:50 +0200

    project restructuring

--- mega_60_days/bonus/bonus1.py
+++
@@ -1,8 +0,0 @@
-title=input("Enter a title: ")
-
-#checking for the length of a function/variable
-length = len(title)
-
-print("The length of the title is ", length)
-
-answers = ['Yes', 'No', 'Yes', 'No', 'No']
